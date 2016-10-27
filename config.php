bc7612f276d6b1a11a59792b56026fa707faee48
m00nfly <cyc2008sky@gmail.com>
2016-10-27 10:59:37 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+$ip = "localhost";  //检测服务器是否 online 的连接参数，若 MaNGOS 和 website 同一台，则可以设定为 localhost;
+$port = "3306";
+$host = "localhost";
+$user = "mangos";
+$pass = "mangos"; 
+$mangoscharacters = "characters";
+$mangosrealm = "realmd";
+$cod = 'utf8';
+?>
